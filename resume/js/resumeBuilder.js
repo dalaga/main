c56a3d3b0b3873c954508ffb754641091ebfd051
dalaga <david.alaga@gmail.com>
2017-05-17 17:08:26 -0400

    uploading resume project

---
+++ resume/js/resumeBuilder.js
@@ -0,0 +1,320 @@
+var bio = {
+    "name": "David Alaga",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile": "202-285-1569",
+        "email": "david.alaga@yahoo.com",
+        "github": "dalaga",
+        "location": "Washington, DC"
+    },
+    "welcomeMessage": "Welcome to my online resume!",
+    "skills": [
+        "HTML5", "CSS3", "JavaScript", "JQuery", "BootStrap",
+        "GitHub"
+    ],
+    "biopic": "images/mypic.png"
+};
+
+var education = {
+    "schools": [{
+            "name": "National University",
+            "location": "Lajolla, CA",
+            "degree": "Masters",
+            "majors": ["Database Adminstrator"],
+            "dates": "2008",
+            "url": "http://www.nationaluniversity.edu"
+        },
+        {
+            "name": "Phoenix University",
+            "location": "Phoenix, AZ",
+            "degree": "Masters",
+            "majors": ["Information Systems / Management"],
+            "dates": "2007",
+            "url": "http://www.phoenix.edu"
+        },
+        {
+            "name": "University of Hawaii",
+            "location": "HI",
+            "degree": "Masters Certificate",
+            "majors": ["TeleComm. Information Resource Management"],
+            "dates": "2007",
+            "url": "http://www.hawaii.edu"
+        },
+        {
+            "name": "University of Palau",
+            "location": "Palau",
+            "degree": "Masters Certificate",
+            "majors": ["Geographic Information Systems (GIS)"],
+            "dates": "2005",
+            "url": "http://www.ppc.palau.edu"
+        },
+        {
+            "name": "DeVry University",
+            "location": "Atlanta, GA",
+            "degree": "Bachelors",
+            "majors": ["BSEET"],
+            "dates": "1996",
+            "url": "http://www.devry.edu"
+        },
+    ],
+
+    "onlineCourses": [{
+            "title": "Frontend Developer Nanodegree",
+            "school": "Udacity, Inc",
+            "dates": "Nov 2016 - Present",
+            "url": "http://www.udacity.com/"
+        },
+        {
+            "title": "Become an Agile Project Manager - LEARNING PATH",
+            "school": "LinkedIn-inLearning",
+            "dates": "Jan 2017",
+            "url": "https://www.linkedin.com/learning"
+        }
+    ]
+};
+
+var work = {
+    "jobs": [{
+            "employer": "Simple Tech Solutions, LLC",
+            "title": "Data Analytics Architect & Full Stack Developer",
+            "location": "Washington DC",
+            "dates": "Jan 2017",
+            "description": "Currently working on Scrum Master Certification"
+        },
+        {
+            "employer": "Malolo Bed and Breakfast",
+            "title": "Business Owner / DBA / Web Developer",
+            "location": "Washington DC",
+            "dates": "Oct 2012 - Present",
+            "description": "Leveraging CRM, GIS and Digital Marketing tools to manage and enhance customer relations, Works with multiple vendors on business applications to help collect data on travel shoppers and analyze their travel and spending patterns., Manage Digital Marketing, Social Media and Business Analytics, Consults as a GIS / DBA SME for Ward 7 Campaign Voters Database with Culver Celeritas on joint venture which includes curation, warehousing, and mining of DC data on voters, Design and create database tables and model using UML, ERD diagram using SQL Developer (Modeler), Toad Data Modeling and SQLPlus., Built and developed GIS mapping Component via APEX 5.1 UI using ArcGIS JavaScript API and Leaflet API., Use ArcGIS online to build map services, ArcGIS Desktop suite (ArcMap and ArcCatalog) to build MXD files and manage the Geodatabases., Use Python as data extraction, scripting tool and for geoprocessing"
+        },
+        {
+            "employer": "VSolvit, LLC",
+            "title": "Program Manager",
+            "location": "Washington, DC",
+            "dates": "Apr 2012 - Oct 2012",
+            "description": "Gathered functional, non-functional, and operational requirements from stakeholders via team meetings and focus groups, Created project management and system design documentation, and application Mash ups, The Quality Manager and Configuration Manager for VSolvit’s CMMI processes and procedures. Manage the scope, implementation, cost, schedule, communications, monitoring, and resource management for GIS services based contractual deliverables through the effective application of planning, tracking, quality assurance, change control, risk management, and process improvement., Participated in business development, managed customer relationships and expectations., Maintained and created Amazon EC2 virtual Mapping environments for development, staging and production application releases., Technical lead assisting development team in creating Map Web Services for custom mapping applications using ESRI’s SOAP and REST services."
+        },
+        {
+            "employer": "Applied Computing Technologies",
+            "title": "GIS Program Manager / SQL DBA - Enterprise Transition Services",
+            "location": "Washington, DC",
+            "dates": "Sept 2011 - Apr 2012",
+            "description": "Managed customer relationships and expectations., Managed the scope, implementation, cost, schedule, communications, monitoring, and resource management for Geospatial Information System (GIS) services-based contractual deliverables through the effective application of planning, tracking, quality assurance, change control, risk management, and process improvement., Participated in business development and staying abreast of the GIS technology trends., Managed the GIS Development team in building map web services in creating custom mapping applications, ***** Analyzed database system that requires the integration of several other systems., Reviewed physical database design, implementation, and testing processes., Created the process database reports to facilitate migration strategies, Defined allocated storage capacity in the design of the database management system (DBMS).,Planed and coordinated the migration of data to newer versions of the DBMS., Recommended performance enhancements, such as increasing storage capacity or modifying interfaces., Analyzed performance data, researches technical solutions, and modifies systems and database"
+        },
+        {
+            "employer": "PenBay Media (Contractor)",
+            "title": "GIS System Analyst",
+            "location": "Washington, DC",
+            "dates": "Jan 2011 - Sept 2011",
+            "description": "Gathered functional, non-functional, and operational requirements from stakeholder and business owners through personal, phone interviews and created / maintained project related documentation., Developed and Built ESRI Mapping Web Services (SOAP and REST) to be used with Prototypes and main client web applications., Acted as technical lead between the client and the development team to help coordinate, documentation, testing and to deliver training to end users."
+        },
+        {
+            "employer": "Blue Raster LLC",
+            "title": "GIS Application Developer",
+            "location": "Washington, DC",
+            "dates": "May 2010 - Oct 2010",
+            "description": "Built applications in Flex 3.5 / 4.0 using remote objects with ColdFusion 9 Servers communicating to Microsoft SQL Server, using ESRI’s Desktop, ESRI’s ArcGIS Server 9.3 and 10., Building custom map web services for Web mapping application data providing REST and SOAP services., Worked with CommonSpot CMS Server in creating CMS mapping applications."
+        },
+        {
+            "employer": "Booz Allen Hamilton",
+            "title": "GIS Application Developer",
+            "location": "Washington, DC",
+            "dates": "Apr 2009 - May 2010",
+            "description": "Developed GIS module was developed using Flex 3 and ESRI Flex API., Designed System Administrator for ESRI’s ArcGIS Server and SDE Oracle Database Schema., Utilized ESRI’s ArcGIS Desktop to Create and Author Web Services., Developed, Tested and work with design team to integrate GIS module into main application via IBM Web Portal and documented the processes., Worked with project managers, team leads, developers and other technical staff to integrate the geospatial web services such as REST and SOAP to build applications solutions for delivery to the client."
+        },
+        {
+            "employer": "AT&T (Contractor)",
+            "title": "GIS Application Developer",
+            "location": "Alpharetta, GA",
+            "dates": "Jul 2008 - Apr 2009",
+            "description": "Managed, Developed, Tested and Debugged each module and widget created using Adobe  Flex Builder 3 and ESRI’s Flex API., Created custom ESRI Web Services for various components / features of mapping applications., • Worked with other vendor mapping web services (Weather, traffic, etc…)  for mapping applications that was integrated to Mcrosoft SQL Server database, Participated in technical reviews with stakeholders and worked closely with technical team in completing deliverables."
+        },
+        {
+            "employer": "American Samoa TeleComm. Authority (ASTCA)",
+            "title": "System Engineer / GIS Engineer",
+            "location": "Pago Pago, AS",
+            "dates": "Apr 2002 - Jul 2008",
+            "description": "Designed and created a geometric network of the land line and fiber customers., Developed and created a WAP Mobile Trouble-call and Service Order applicationsWorked with ArcSDE 9.2, ArcIMS 9.2, ArcEngine 9.2 and ArcGIS Server 9.2., Utilized Microsoft SQL Server 2005 and .NET Framework and JAVA (J2SE and J2EE)., Utilized a combination of JAVA (J2EE) and COLDFUSION server technology via ArcPad on Trimble GeoXt GPS Units., Worked with management and technical teams in optimizing local telephone system mobile and land network."
+        },
+        {
+            "employer": "Ajilon",
+            "title": "Broadband Access Network / Project Manager (Contractor)",
+            "location": "High Point, NC",
+            "dates": "Dec 2000 - Mar 2002",
+            "description": "Planned, designed, coordinated, commissioned, provisioned, and tested Access Network nodes for clients telephone networks., Worked with management and technical teams in optimizing local telephone system mobile and land network."
+        },
+        {
+            "employer": "Nortel Networks",
+            "title": "System Engineer",
+            "location": "Alpharetta, GA",
+            "dates": "Nov 1996 - Nov 1999",
+            "description": "Worked with design engineers, and vendors in designing, developing and implementing telecommunications applications for customers"
+        },
+        {
+            "employer": "Intercept Technologies, Inc",
+            "title": "CAD operator",
+            "location": "Alpharetta, GA",
+            "dates": "Jun 1996 - Oct 1996",
+            "description": "Coordinated with customers on design and implementation of printed circuit board specifications."
+        },
+        {
+            "employer": "Sable Communications",
+            "title": "Scheduler",
+            "location": "Atlanta, GA",
+            "dates": "Nov 1992 - Oct 1996",
+            "description": "Protégé for the TRW’s E911 project for the city of Atlanta prepping for the 1996 Olympics."
+        },
+        {
+            "employer": "United States Marine Corps",
+            "title": "Telecommunications Specialist",
+            "location": "Jacksonville, NC",
+            "dates": "Sept 1984 - sept 1992",
+            "description": "Managed, designed, implemented, monitored, maintained operations of Copper and Fiber plant facilities., Marine Security Embassy Guard Duty (MSG) (Top Clearance)"
+        }
+    ]
+};
+
+var projects = {
+    "projects": [{
+        "title": "Malolo Bed and Breakfast, Washington DC",
+        "dates": "Oct 2012 - Present",
+        "description": "Created and designed Internet Presence, Digital Marketing and manage relationships with travel partners.",
+        "images": ["http://placehold.it/200x100", "http://placehold.it/100x100"]
+    }]
+};
+
+bio.display = function() {
+
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+    var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+    $('#header').prepend(formattedMessage);
+    $('#header').prepend(formattedPic);
+    $('#header').prepend(formattedRole);
+    $('#header').prepend(formattedName);
+
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+
+    var idStrings = ['#topContacts', '#footerContacts'];
+    for (var i = 0, id; i < idStrings.length; i++) {
+        id = idStrings[i];
+        $(id).append(formattedMobile);
+        $(id).append(formattedEmail);
+        $(id).append(formattedGitHub);
+        $(id).append(formattedLocation);
+    }
+
+    $('#header').append(HTMLskillsStart);
+//Display skills in a for loop
+    for (var b = 0; b < bio.skills.length; b++) {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[b]);
+        $('#skills').append(formattedSkill);
+}
+};
+
+bio.display();
+
+work.display = function() {
+    for (j = 0; j < work.jobs.length; j++) {
+        $('#workExperience').append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[j].employer);
+        var formatttedTitle = HTMLworkTitle.replace("%data%", work.jobs[j].title);
+        var formattedEmployerTitle = formattedEmployer +
+            formatttedTitle;
+        $('.work-entry:last').append(formattedEmployerTitle);
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[j].dates);
+        $('.work-entry:last').append(formattedDates);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[j].description);
+        $('.work-entry:last').append(formattedDescription);
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[j].location);
+        $('.work-entry:last').append(formattedLocation);
+    }
+};
+
+work.display();
+
+education.display = function() {
+    for (e = 0; e < education.schools.length; e++) {
+        $('#education').append(HTMLschoolStart);
+
+        var formattedName = HTMLschoolName.replace("%data%", education.schools[e].name);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[e].degree);
+        $('.education-entry:last').append(formattedName + formattedDegree);
+        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[e].location);
+        $('.education-entry:last').append(formattedLocation);
+        var formattedDate = HTMLschoolDates.replace("%data%", education.schools[e].dates);
+        $('.education-entry:last').append(formattedDate);
+        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[e].majors);
+        $('.education-entry:last').append(formattedMajor);
+        var formattedEducationURL = HTMLonlineURL.replace("%data%", education.schools[e].url);
+        $('.education-entry:last').append(formattedEducationURL);
+    }
+    $('#education').append(HTMLonlineClasses);
+    for (i = 0; i < education.onlineCourses.length; i++) {
+        $('#education').append(HTMLschoolStart);
+
+        var formattedtitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title);
+        var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
+        var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+        var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url);
+        $('.education-entry:last').append(formattedSchool, formattedtitle, formattedDates, formattedURL);
+    }
+
+};
+
+education.display();
+
+projects.display = function() {
+    //display code goes here
+    for (p = 0; p < projects.projects.length; p++) {
+        $('#projects').append(HTMLprojectStart);
+
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[p].title);
+        $('.project-entry:last').append(formattedTitle);
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[p].dates);
+        $('.project-entry:last').append(formattedDates);
+        var formattedDescription = HTMLworkDescription.replace("%data%", projects.projects[p].description);
+        $('.project-entry:last').append(formattedDescription);
+        /* var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[p].images);
+         $('.project-entry:last').append(formattedImage);*/
+    }
+    var proj = projects.projects;
+    //images array looped using for loop
+    for (var i = 0; i < proj.length; i++) {
+        var images = proj[i].images;
+        for (var x = 0; x < images.length; x++) {
+            var formattedImage = HTMLprojectImage.replace("%data%", images[i]);
+            $('.project-entry:last').append(formattedImage);
+        }
+    }
+};
+
+projects.display();
+
+$(document).click(function(loc) {
+    var x = loc.pageX;
+    var y = loc.pageY;
+
+    logClicks(x, y);
+});
+
+function locationizer(work_obj) {
+    var locationArray = [];
+
+    for (j = 0; j < work_obj.jobs.length; j++) {
+        var newLocation = work_obj.jobs[j].location;
+        locationArray.push(newLocation);
+    }
+    return locationArray;
+}
+
+locationizer(work);
+
+$('#mapDiv').append(googleMap);
